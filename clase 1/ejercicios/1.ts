6a8737bf0ddea175ed902ef9094d247cd40b28e3
JhonnyyDev <Jhonnyydev@gmail.com>
2025-06-04 18:26:08 -0300

    ejercicios clase 1 y 2

---
+++ clase 1/ejercicios/1.ts
@@ -0,0 +1,7 @@
+//Para hacer funcionar TS en consola directamente, se debe instalar
+//tsx, mas moderno que ts-node que se usaba anteriormente
+//npm -install -g tsx
+//luego para abrir archivos seria de la siguiente manera:
+//tsx 1.ts
+
+console.log("hola desde TS")
---
+++ clase 2/ejercicios/1.ts
@@ -0,0 +1,11 @@
+// let pelota: number;
+// console.log(pelota);
+
+// esto da undefined
+
+// const numero: unknown = 5;
+// const otroNumero: number = 3;
+// const suma = numero + otroNumero;
+
+// Esto da error de compilacion
+
